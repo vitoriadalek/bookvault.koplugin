53c70d1aadd37b3f593a0db9bd5831acfdb82eab
Vitoria <166748655+vitoriadalek@users.noreply.github.com>
2026-09-16 23:38:51 -0300

    release: BookVault 1.3.1

--- _meta.lua
+++ _meta.lua
@@ -2,5 +2,5 @@ return {
     name = "bookvault",
     fullname = "BookVault",
     description = "Virtual reading-status library with native cover mosaic, private content and protected folders.",
-    version = "1.3.0",
+    version = "1.3.1",
 }
