8675f432422610505542f7f6c16f5ce547ad86d1
Vitoria <166748655+vitoriadalek@users.noreply.github.com>
2026-09-17 01:36:03 -0300

    Bump BookVault to 1.6.0

--- _meta.lua
+++ _meta.lua
@@ -1,6 +1,6 @@
 return {
     name = "bookvault",
     fullname = "BookVault",
-    description = "Reading-status library with customizable visible categories, private content and password-protected folders.",
-    version = "1.5.0",
+    description = "Reading-status library with KOReader collections, private content and password-protected folders.",
+    version = "1.6.0",
 }
